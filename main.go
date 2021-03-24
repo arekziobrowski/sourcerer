431664d8decaa67236d134b93769119bf02dd15b
Arkadiusz Ziobrowski <arkadiusz.ziobrowski@ingrid.com>
2021-03-24 23:53:35 +0100

    get dependencies

--- main.go
+++ main.go
@@ -1,5 +1,10 @@
 package main
 
 func main() {
-
+	/*
+		1. git init
+		2. git remote add origin git@github.com:go-git/go-git.git
+		3. git fetch origin ef33fff761a2fabb7f0daf0c1779d2dfac1056da --depth=1
+		4. git reset --hard FETCH_HEAD
+	*/
 }
